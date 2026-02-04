fa67461e1ac72b74372a74570976bdc4bea1c270
JasperJCHO <juichi.ho@gmail.com>
2026-02-04 14:54:10 +0800

    Add routes for custom object data handling
    Updated index.js to include environment variable for private app access token and added routes for fetching and updating custom object data.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,19 +9,62 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+// Custom Object ID for Sample Request
+const CUSTOM_OBJECT_TYPE = '2-221479768';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const samplesUrl = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_TYPE}?properties=sample_request_number,apply_date,customer_need_date`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+    try {
+        const resp = await axios.get(samplesUrl, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Sample Requests | HubSpot Custom Objects', data });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching Sample Request data');
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const newSample = {
+        properties: {
+            sample_request_number: req.body.sample_request_number,
+            apply_date: req.body.apply_date,
+            customer_need_date: req.body.customer_need_date
+        }
+    };
+
+    const createSampleUrl = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_TYPE}`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        await axios.post(createSampleUrl, newSample, { headers });
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error creating new Sample Request');
+    }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
